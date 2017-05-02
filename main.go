02d7cb57736b956a9367752615b3ee5319fe2e5f
tagasouichiro <stitow14@gmail.com>
2017-05-02 21:28:39 +0900

    Add sample multiplication

--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strconv"
+)
 
+func main() {
+	if err := parse("10*2"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func parse(str string) error {
+	expr, err := parser.ParseExpr(str)
+	if err != nil {
+		return err
+	}
+
+	ast.Inspect(expr, func(n ast.Node) bool {
+		if n != nil {
+			fmt.Printf("%[1]v(%[1]T)\n", n)
+		}
+		return true
+	})
+
+	if e, ok := expr.(*ast.BinaryExpr); ok {
+		if v, err := eval(e); err == nil {
+			fmt.Println(v)
+			return nil
+		}
+		return err
+	}
+	return errors.New("string is not *ast.BinaryExpr")
+}
+
+func eval(expr *ast.BinaryExpr) (int64, error) {
+	xLit, ok := expr.X.(*ast.BasicLit)
+	if !ok {
+		return 0, errors.New("left operand faild")
+	}
+
+	yLit, ok := expr.Y.(*ast.BasicLit)
+	if !ok {
+		return 0, errors.New("right operand faild")
+	}
+
+	if expr.Op != token.MUL {
+		return 0, errors.New("avaliable operator is multiple")
+	}
+
+	x, err := strconv.ParseInt(xLit.Value, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	y, err := strconv.ParseInt(yLit.Value, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return x * y, nil
 }
